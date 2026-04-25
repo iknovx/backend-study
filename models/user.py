bc79b109fce7dc8d62c0053cb5e621ebbba1b8ed
lmkptr <dinqfh@gmail.com>
2026-04-25 22:42:15 +0200

    new

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 users = [
     {"id": 1, "name": "John", "age": 30, "student": True},
     {"id": 2, "name": "Jane", "age": 25, "student": False},
-    {"id": 3, "name": "Doe", "age": 22, "student": True}
+    {"id": 3, "name": "Doe", "age": 22, "student": True},
+    {"id": 4, "name": "Alice", "age": 28, "student": False}
 ]
